fd6b73e6e2ee54bac72415dbfa6e2ac4eba89f6a
danasilver <dsilver1221@gmail.com>
2014-01-07 00:01:45 -0500

    Ughh! (Grunt.js) :boar:

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/* <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'static/assets/<%= pkg.name %>.min.js': ['static/js/midd-menu.js']
+        }
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'app/**/*.js', 'static/js/*.js', 'config/**/*.js', 'lib/*.js', 'server.js'],
+      options: {
+        jshintrc: '<%= pkg.jshintConfig %>'
+      }
+    },
+    less: {
+      development: {
+        files: {
+          'static/assets/main.css': 'static/less/main.less'
+        }
+      },
+      production: {
+        options: {
+          cleancss: true,
+          compress: true
+        },
+        files: {
+          'static/assets/main.css': 'static/less/main.less'
+        }
+      }
+    },
+    nodemon: {
+      def: {
+        options: {
+          file: 'server.js',
+          nodeArgs: ['--debug']
+        }
+      }
+    },
+    watch: {
+      files: ['*.js', '*.less'],
+      tasks: ['jshint', 'uglify', 'less', 'nodemon']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('default', ['jshint', 'uglify', 'less'])
+};
